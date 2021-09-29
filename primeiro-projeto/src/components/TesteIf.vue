28fdc0168418035ac571d1236e41e540afd82e07
FernandoNunesRizzato <fernandorizzato@outlook.com>
2021-09-29 15:43:54 -0300

    Teste If, NovoComponente e início TesteFor

---
+++ primeiro-projeto/src/components/TesteIf.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="teste-if">
+    <h1 v-if="podeVer">Teste 1</h1>
+    <h1 v-else>Teste 2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "testeif",
+  data() {
+    return {
+      podeVer: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
